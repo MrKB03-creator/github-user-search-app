3bdafe7d9764a1bdaa8da04594211f361fd0f702
MrKB03-creator <badillokurt9@gmail.com>
2024-10-11 18:22:01 -0700

    JS code done and css touchups needs it

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import  { useState } from 'react';
 import Light_mode from "./components/Light_mode"
 import Dark_mode from "./components/Dark_mode"
 
 function App() {
-  return (
-    <>
-      <Dark_mode />
-      <Light_mode />
+
+
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleMode = () => {
+      setIsDarkMode(!isDarkMode);
+    };
+
+    return (
+      <>
+      {isDarkMode ? <Dark_mode toggleMode={toggleMode} /> : <Light_mode toggleMode={toggleMode} />}
     </>
-  )
+    );
 }
 
 export default App
--- src/assets/Images/index.tsx
+++ src/assets/Images/index.tsx
@@ -1,6 +1,5 @@
 import iconCompany from './icon-company.svg';
 import iconLocation from './icon-location.svg';
-import iconMoon from './icon-moon.svg';
 import iconSearch from './icon-search.svg';
 import iconSun from './icon-sun.svg';
 import iconTwitter from './icon-twitter.svg';
@@ -11,7 +10,6 @@ import githubIconDarkmode from './github-mark _Darkmode.svg'
 export {
     iconCompany,
     iconLocation,
-    iconMoon,
     iconSearch,
     iconSun,
     iconTwitter,
